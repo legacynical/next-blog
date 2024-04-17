05a14fb9a47c18dc96e1def39d3080e5ff3f9cc1
Legacynical <legacynical@gmail.com>
2024-04-17 16:42:03 -0700

    Test darkmode

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,12 +1,20 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./themeToggle.module.css";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 
 const ThemeToggle = () => {
+  const { theme } = useContext(ThemeContext);
+
+  console.log(theme);
+
   return (
     <div className={styles.container}>
       <Image src="/moon.png" alt="moon" width={14} height={14} />
       <div className={styles.ball}></div>
-      <Image src="/sun.png" alt="moon" width={14} height={14} />
+      <Image src="/sun.png" alt="sun" width={14} height={14} />
     </div>
   );
 };
---
+++ src/providers/ThemeProvider.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { ThemeContext } from "@/context/ThemeContext";
+import React, { useContext } from "react";
+
+export const ThemeProvider = ({ children }) => {
+  const { theme } = useContext(ThemeContext);
+  return <div className={theme}>{children}</div>;
+};
+
+export default ThemeProvider;
