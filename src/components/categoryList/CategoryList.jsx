a20a17e261e80370f6dc9e4045144fb3e37e160d
Legacynical <legacynical@gmail.com>
2024-04-16 18:21:05 -0700

    Add more components

---
+++ src/components/categoryList/CategoryList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './categoryList.module.css'
+
+export const CategoryList = () => {
+  return (
+    <div className={styles.container}>CategoryList</div>
+  )
+}
+
+export default CategoryList
