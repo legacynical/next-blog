5a57f468a626c3dbf340f24baf3e7b6cddf9c06a
Legacynical <legacynical@gmail.com>
2024-04-16 17:39:19 -0700

    Set media screen breakpoints and initialize Navbar component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
   <div>
     <Link href="/">
+      <h1>Bee Movie Script</h1>
       According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little.
     </Link>
   </div>
