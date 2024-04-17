f29339242395967cdc7613e31d38fb71ac63878a
Legacynical <legacynical@gmail.com>
2024-04-17 14:58:10 -0700

    Add themeToggle element

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
 import styles from "./authLinks.module.css";
 
 const AuthLinks = () => {
-  return <div className={styles.container}>AuthLinks</div>;
+  // temporary
+  const status = "authenticated";
+  return (
+    <>
+      {status === "notauthenticated" ? (
+        <Link href="/login">Login</Link>
+      ) : (
+        <>
+          <Link href="/write">Write</Link>
+          <span className={styles.link}>Logout</span>
+        </>
+      )}
+    </>
+  );
+
+  // return <div className={styles.container}>AuthLinks</div>;
 };
 
 export default AuthLinks;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,12 +9,12 @@ export const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.social}>
-        <image src="/youtube.png" alt="youtube" width={24} height={24} />
-        <image src="/facebook.png" alt="facebook" width={24} height={24} />
-        <image src="/tiktok.png" alt="tiktok" width={24} height={24} />
-        <image src="/instagram.png" alt="instagram" width={24} height={24} />
+        <Image src="/youtube.png" alt="youtube" width={24} height={24} />
+        <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+        <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
+        <Image src="/instagram.png" alt="instagram" width={24} height={24} />
       </div>
-      <div className={styles.logo}></div>
+      <div className={styles.logo}>Legacynical</div>
       <div className={styles.links}>
         <ThemeToggle />
         <Link href="/">Homepage</Link>
